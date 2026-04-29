a0103a4cb5b215e37c7057ca45c54f043f6fba39
elvistaver <elvistaver@gmail.com>
2026-04-29 20:25:04 +0100

    ascil

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ import (
 
 func main() {
 
-	if len(os.Args)!= 3{
+	if len(os.Args) != 3 {
 		fmt.Println("invalid commandline argument: usage; go run . input text  bannerfile-type")
 		return
 	}
 	input := strings.TrimSpace(os.Args[1])
-	banner := (os.Args[2])
+	FileName := (os.Args[2])
 
 	if input == "" {
 		return
@@ -25,18 +25,18 @@ func main() {
 		return
 	}
 
-	files := Filesystem.ReadBanner(banner)
-
-	for i := 0; i < 8; i++ {
-		line := ""
-		for _, ch := range input {
-			if ch < 32 || ch > 126 {
-				log.Fatal(" \n not a valid character")
-				return
+	files := Filesystem.ReadBanner(FileName)
+		for i := 0; i < 8; i++ {
+			line := ""
+			for _, ch := range input {
+				if ch < 32 || ch > 126 {
+					log.Fatal(" \n not a valid character")
+					return
+				}
+				start := (int(ch)-32)*9 + 1
+				line += files[start+i]
 			}
-			start := (int(ch)-32)*9 + 1
-			line += files[start+i]
+			fmt.Print(line, "\n")
 		}
-		fmt.Print(line, "\n")
-	}
+	
 }
